32ef26f6640af37aff7ddbf15b0002f08610053b
Oswaldo-Alexander <mercadorodarte123@gmail.com>
2026-01-30 00:34:49 -0600

    agregando un comentario

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+// librerias necesarias 
 #include <windows.h>
 #include <iostream>
 #include <conio.h>
 
+// el main del archivo
 int main()
 {
+	// cout es para imprimir un mensaje
 	std::cout << "hola mundo en c++" << std::endl;
+	// getch hace que no se cierre solo el programa
 	getch();
 }
